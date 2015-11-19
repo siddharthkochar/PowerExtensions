093e5c2f2e22d99382ac7044cf3c24b7f47db144
sidkcr <sidkcr@gmail.com>
2015-11-19 17:17:16 +0530

    1. Added 'OrDefault', 'In' extensions. 2. Modified IsNull extension

--- PowerExtensions/ObjectExtensions.cs
+++ PowerExtensions/ObjectExtensions.cs
@@ -1,11 +1,24 @@
-﻿
+﻿using System;
+using System.Linq;
+
 namespace PowerExtensions
 {
     public static class ObjectExtensions
     {
-        public static bool IsNull(this object obj)
+        public static bool IsNull<T>(this T source)
         {
-            return obj == null;
+            return null == source;
+        }
+
+        public static bool In<T>(this T source, params T[] list)
+        {
+            if (null == source) throw new ArgumentNullException("source");
+            return list.Contains(source);
+        }
+
+        public static T OrDefault<T>(this T source, T defaultValue)
+        {
+            return null == source ? defaultValue : source;
         }
 
     }
