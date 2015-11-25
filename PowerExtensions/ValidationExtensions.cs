6f3ccf6e0ebaba77804f702feaefd4ebd31d941c
Siddharth Kochar <sidkcr@gmail.com>
2015-11-25 15:28:34 +0530

    Added IsValid and ValidationResults methods for Attribute Validations.

---
+++ PowerExtensions/ValidationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PowerExtensions
+{
+    public static class ValidationExtensions
+    {
+        private static List<ValidationResult> Errors { get; set; }
+
+        public static bool IsValid<T>(this T source)
+        {
+            var context = new ValidationContext(source);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(source, context, results, true);
+            Errors = results;
+            return isValid;
+        }
+
+        public static List<ValidationResult> ValidationResults<T>(this T source)
+        {
+            return Errors;
+        }
+    }
+}
